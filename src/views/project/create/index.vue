d11fd60ac934da51da884b57ba268529268c5619
MapleWan <1041709112@qq.com>
2025-12-22 10:54:15 +0800

    feat: markdown 编辑器引入测试

--- src/views/project/create/index.vue
+++ src/views/project/create/index.vue
@@ -1,13 +1,18 @@
 <script setup>
-import useMainStore from '@/stores/main';
-const mainStore = useMainStore();
-
+import useMainStore from '@/stores/main'
+const mainStore = useMainStore()
+import Editor from '@/components/Editor/index.vue'
+import { watch, ref } from 'vue'
+const content = ref('')
+watch(content, (newValue) => {
+  console.log(newValue)
+})
 </script>
 <template>
   <div class="create-container">
     创作 {{ mainStore.currentProjectId }}
+    <Editor v-model="content" />
   </div>
 </template>
-
 
 <style scoped></style>
