ff44394a0b447d5dde23b0b2da4d9a111b4edb51
MapleWan <1041709112@qq.com>
2025-12-22 23:38:42 +0800

    feat: 路由守卫引入 NProgress 进度条

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ProjectLayout from '@/components/layout/ProjectLayout/index.vue'
+import { getToken } from '@/utils/auth';
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+NProgress.configure({ showSpinner: false });
+
 const defaultRouterList = [
   {
     path: '/dashboard',
@@ -44,9 +49,26 @@ const defaultRouterList = [
   }
 ]
 
+const whiteList = ["/login", "/register"];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [...defaultRouterList],
 })
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  if (whiteList.includes(to.path)) {
+    next();
+  } else {
+    const token = getToken();
+    if (token) {
+      next();
+    } else {
+      next("/login");
+    }
+  }
+  NProgress.done();
+});
 
 export default router
