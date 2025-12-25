4839232829bb1ac75ae6e794968e2a1ca169ff9a
MapleWan <1041709112@qq.com>
2025-12-25 16:14:09 +0800

    feat(ProjectListTable): 添加项目列表行点击跳转功能
    - 引入 vue-router 和 mainStore 依赖
    - 添加 handleRowClick 方法处理行点击事件
    - 点击行时设置当前项目ID并跳转到项目仪表板
    - 为表格行添加 cursor-pointer 样式类
    - 集成行点击事件监听器到表格组件

--- src/views/dashboard/components/ProjectListTable.vue
+++ src/views/dashboard/components/ProjectListTable.vue
@@ -1,17 +1,26 @@
 <script setup>
 import NoDataIcon from "@/assets/icons/svg/noData.svg"
+import { useRouter } from 'vue-router'
+import useMainStore from '@/stores/main'
 
+const router = useRouter()
+const mainStore = useMainStore()
 const props = defineProps({
   projectList: {
     type: Array,
     required: true
   }
 })
+function handleRowClick(row) {
 
+  mainStore.setCurrentProjectId(row.id)
+  router.push({ name: 'projectDashboard' })
+}
 </script>
 <template>
   <div class="flex flex-wrap gap-4 p-t-4">
-    <el-table :data="projectList" style="width: 100%" v-if="projectList.length > 0" max-height="800">
+    <el-table :data="projectList" style="width: 100%" v-if="projectList.length > 0" max-height="800"
+      row-class-name="cursor-pointer" @row-click="handleRowClick">
       <el-table-column prop="name" fixed label="项目名" min-width="180" sortable />
       <el-table-column prop="updated_at" label="更新时间" width="180" sortable />
       <el-table-column prop="created_at" label="创建时间" width="180" sortable />
